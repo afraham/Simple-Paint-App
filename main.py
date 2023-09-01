7ba476890e250b758c6d77c9b897a5b3b9d305a1
afrah <afrahamerriarh@hotmail.com>
2023-08-31 22:43:31 -0600

    added everything to respective classes

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from paint_and_erase import PaintAndErase
 # primary functionality / class for the paint application
 class BasicPaintApp:
     def __init__(self, root):
-        self.root()
+        self.root = root
         self.root.title("(Basic) Paint App")
         self.root.geometry("700x700")
     
@@ -18,64 +18,34 @@ class BasicPaintApp:
         self.brush_settings = BrushSettings()
         self.paint_and_erase = PaintAndErase(self.canvas, self.brush_settings)
 
+        brush_sizes = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+        # paintbrush size label
         brush_size_label = Label(root, text="Brush Size: ")
         brush_size_label.pack()
+
+        clicked_brush = IntVar()
+        clicked_brush.set(self.brush_settings.brush_size)
+
+        drop_brush = OptionMenu(self.root, clicked_brush, *brush_sizes, command=self.brush_settings.change_brush_size)
+        drop_brush.pack()
+
+        self.canvas.bind("<B1-Motion>", self.paint_and_erase.paint)
+
+        # eraser size label
+        eraser_size_label = Label(root, text="Eraser Size: ")
+        eraser_size_label.pack()
+
+        clicked_erase = IntVar()
+        clicked_erase.set(self.brush_settings.erase_size)
+
+        drop_eraser = OptionMenu(self.root, clicked_erase, *brush_sizes, command=self.brush_settings.change_erase_size)
+        drop_eraser.pack()
+
+        self.canvas.bind("<B3-Motion>", self.paint_and_erase.erase)
 
 # main
 if __name__ == "__main__":
     root = Tk()
     app = BasicPaintApp(root)
     root.mainloop()
-
-
-
-""" root = Tk()
-root.title("(Basic) Paint App")
-root.geometry("700x700")
-
-canvas = tk.Canvas(root, bg="white")
-canvas.pack(fill="both", expand=True)
-
-brush_size_label = Label(root, text="Brush Size: ")
-brush_size_label.pack()
-
-# Dropdown menu options
-brush_width = 3
-def brush_selection(selection):
-        global brush_width
-        brush_width = selection
-
-brush_sizes = [
-    1,
-    2,
-    3,
-    4,
-    5, 
-    6,
-    7,
-    8,
-    9,
-    10
-]
-  
-# datatype of menu text
-clicked = IntVar()
-clicked.set(brush_width)
-
-# Create Dropdown menu
-drop = OptionMenu(root, clicked, *brush_sizes, command=brush_selection)
-drop.pack()
-
-def paint(event):
-    x, y = event.x, event.y
-    canvas.create_oval(x-2, y-2, x+2, y+2, fill="black", width=brush_width)
-
-canvas.bind("<B1-Motion>", paint)
-
-def erase(event):
-    x, y = event.x, event.y
-    canvas.create_oval(x-2, y-2, x+2, y+2, fill="white", outline="white", width=brush_width)
-
-canvas.bind("<B3-Motion>", erase)
- """
-root.mainloop()
--- paint_and_erase.py
+++ paint_and_erase.py
@@ -7,7 +7,7 @@ class PaintAndErase:
     def paint(self, event):
         x, y = event.x, event.y
         self.canvas.create_oval(x-2, y-2, x+2, y+2, 
-                                fill=self.brush_settings.brush_colour, 
+                                fill=self.brush_settings.paint_colour, 
                                 width=self.brush_settings.brush_size)
     
     def erase(self, event):
