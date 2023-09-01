2fecfd9c01ffe398ea8d2d137c00ba2c19e7469b
afrah <afrahamerriarh@hotmail.com>
2023-08-31 22:55:10 -0600

    added documentation

--- brush_settings.py
+++ brush_settings.py
@@ -1,17 +1,37 @@
 # class for brush settings, including the size (of the paintbrush and eraser) and colour of the paintbrush
 class BrushSettings:
+    """
+    This class manages brush settings for the Tkinter painting application.
+    Allows changing brush size, eraser size, and brush color.
+    """
     def __init__(self):
+        """
+        Initialize brush settings with default values.
+        """
         self.brush_size = 3
         self.erase_size = 3
         self.paint_colour = "black"
     
     def change_brush_size(self, size):
+        """
+        Changes the size of the brush.
+        args:
+            size (int): new brush size
+        """
         self.brush_size = size
 
     def change_erase_size(self, size):
+        """
+        Changes the size of the eraser.
+        args:
+            size (int): new eraser size
+        """
         self.erase_size = size
     
     def change_brush_colour(self, colour):
+        """
+        Changes the colour of the brush.
+        args:
+            colour (str): new brush color
+        """
         self.paint_colour = colour
-
-    
--- paint_and_erase.py
+++ paint_and_erase.py
@@ -1,16 +1,35 @@
 # class for the logic behind the paintbrush and eraser
 class PaintAndErase:
+    """
+    This class handles the brush logic for the Tkinter painting application.
+    """
     def __init__(self, canvas, brush_settings):
+        """
+        Initialize the PaintAndErase object.
+        args:
+            canvas (tkinter.Canvas): the canvas on which to paint and erase
+            brush_settings (BrushSettings): the brush settings to use for painting and erasing
+        """
         self.canvas = canvas
         self.brush_settings = brush_settings
 
     def paint(self, event):
+        """
+        Paint on the canvas using the current brush settings.
+        args:
+            event: the event containing the mouse position
+        """
         x, y = event.x, event.y
         self.canvas.create_oval(x-2, y-2, x+2, y+2, 
                                 fill=self.brush_settings.paint_colour, 
                                 width=self.brush_settings.brush_size)
     
     def erase(self, event):
+        """
+        Erase on the canvas using the current brush settings.
+        args:
+            event: the event containing the mouse position
+        """
         x, y = event.x, event.y
         self.canvas.create_oval(x-2, y-2, x+2, y+2, 
                                 fill="white", 
